3da12686d5fe65b4286d4a140481d5da8a87abb5
pi <pi@lorem>
2019-11-06 12:20:22 +0100

    second commit

--- lib.py
+++ lib.py
@@ -13,13 +13,15 @@ class Block:
 		else:
 			return random.choice(value_bad)
 
-	def __init__(self, bad): 
-		self.value = self.get_random_value(bad)
-		hash_ = sum([ord(s) for s in self.value]) % 10
+	def init(self, bad): 
+		#self.value = self.get_random_value(bad)
+		#self.hash = sum([ord(s) for s in self.value]) % 1000
+		self.value = ''.join([random.choice('0123456789abcdef') for i in range(0, 10)])
+		self.hash = 0
 		self.block_number = -1
 		self.bad = bad
 
-	def copy():
+	def copy(self):
 		b = Block()
 		b.value = self.value
 		b.hash = self.hash
@@ -32,11 +34,21 @@ class Chain:
 		self.blocks = []
 
 	def add(self, bad):
-		self.blocks = self.blocks[-4:]
-		block = Block(bad)
+		#self.blocks = self.blocks[-4:]
+		block = Block()
+		block.init(bad)
 		block.block_number = len(self.blocks)
 		self.blocks.append(block) 
 
+	def length(self):
+		return len(self.blocks)
+
+	def copy(self):
+		c = Chain()
+		for block in self.blocks:
+			b = block.copy()
+			c.blocks.append(b)
+		return c
 
 class Computer:
 	def __init__(self, stdscr, line, col):
@@ -44,6 +56,7 @@ class Computer:
 		self.stdscr = stdscr
 		self.line = line
 		self.col = col
+		self.linked_to = []
 
 	def draw(self):
 		self.stdscr.addstr(self.line*6+0, self.col*14, '┌──────────┐')
@@ -51,15 +64,67 @@ class Computer:
 		self.stdscr.addstr(self.line*6+2, self.col*14, '│          │')
 		self.stdscr.addstr(self.line*6+3, self.col*14, '│          │')
 		self.stdscr.addstr(self.line*6+4, self.col*14, '└──────────┘')
+		for l in self.linked_to:
+			if (l.line == self.line + 1) and (l.col == self.col):
+				self.stdscr.addstr(self.line*6+5, self.col*14+6, '│')
+			if (l.line == self.line) and (l.col == self.col + 1):
+				self.stdscr.addstr(self.line*6+2, self.col*14+12, '──') 
 
 	def print_content(self):
 		i = 0
-		for block in self.chain.blocks[-5:]:
+		for block in self.chain.blocks[-3:]:
+			self.stdscr.addstr(self.line*6+1+i, self.col*14+1, '          ')
 			if block.bad:
-				self.stdscr.addstr(self.line*6+1, self.col*14+1+i, block.value[0:10], curses.color_pair(1))
+				self.stdscr.addstr(self.line*6+1+i, self.col*14+1, block.value[0:10], curses.color_pair(1))
 			else: 
-				self.stdscr.addstr(self.line*6+1, self.col*14+1+i, block.value[0:10])
+				self.stdscr.addstr(self.line*6+1+i, self.col*14+1, block.value[0:10])
 			i = i + 1
+		self.stdscr.addstr(self.line*6+1, self.col*14+7, '%4d' % self.chain.length())
+		bad = False
+		for block in self.chain.blocks:
+			if block.bad:
+				bad = True
+		self.stdscr.addstr(self.line*6+1, self.col*14+7, '%4d' % self.chain.length())
+		self.stdscr.addstr(self.line*6+0, self.col*14, '┌──────────┐')
+		if bad:
+			self.stdscr.addstr(self.line*6, self.col*14+4, 'fail', curses.color_pair(1))
+		else:
+			self.stdscr.addstr(self.line*6, self.col*14+4, 'ok')
+
+	def copy_from_neighbour(self, neighbour): 
+		for block in neighbour.chain.blocks[self.new_chain.length():]:
+			self.new_chain.blocks.append(block.copy())
+
+	def copy_from_neighbour_if_ok(self, neighbour):
+		if self.new_chain.length() == 0:
+			self.copy_from_neighbour(neighbour)
+		else:
+			if neighbour.new_chain.blocks[self.new_chain.length() - 1].hash == self.new_chain.blocks[-1].hash:
+				self.copy_from_neighbour(neighbour)
+
+	def copy_from_neighbour(self, neighbour):
+		self.new_chain = neighbour.chain.copy()
+
+	def write_line(self, i, s):
+		self.stdscr.addstr(self.line*6+1+i, self.col*14+1, s)
+
+	def get_content_from_neighours(self):
+		self.new_chain = self.chain.copy()
+		i = 0
+		best = None
+		best_length = self.new_chain.length()
+		for comp in self.linked_to:
+			#self.write_line(i, "%d %d" % (comp.chain.length(), best_length))
+			if comp.chain.length() > best_length:
+				best = comp
+				best_length = comp.chain.length()
+			i = i + 1
+
+		if best != None:
+			self.copy_from_neighbour(best)
+
+	def time_shift(self):
+		self.chain = self.new_chain
 
 class Computers:
 	def __init__(self, stdscr):
@@ -78,11 +143,37 @@ class Computers:
 	def print_content(self):
 		for i in range(self.count):
 			self.list[i].print_content()
-	
+
+	def propagate_content(self):
+		for comp in self.list:
+			comp.get_content_from_neighours()
+
+	def time_shift(self): 
+		for comp in self.list:
+			comp.time_shift()
+
+	def add_random(self, bad):
+		random.choice(self.list).chain.add(bad)
+
 class Links:
-# will apply some random links to all computers
-# will apply first all links
-# then remove some
-# then apply to each computers
-# each comp will draw its own links
-# dotted line for links
+	def __init__(self, computers):
+		self.links = []
+		for i in range(0, computers.lines):	
+			for j in range(0, computers.cols):	
+				comp = computers.list[i * computers.cols + j]
+				if j < computers.cols - 1:
+					comp_to_the_right = computers.list[i * computers.cols + j + 1]
+					self.links.append((comp, comp_to_the_right))
+				if i < computers.lines - 1:
+					comp_underneath = computers.list[(i + 1) * computers.cols + j] 
+					self.links.append((comp, comp_underneath))
+
+	def remove_some(self, count = 40):
+		for i in range(0, count):
+			self.links.pop(random.randrange(0, len(self.links)))
+
+
+	def apply(self, computers):
+		for link in self.links:
+			link[0].linked_to.append(link[1])
+			link[1].linked_to.append(link[0])
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import lib
 import curses
+import time
 
 
 #https://docs.python.org/3/howto/curses.html
@@ -11,12 +12,42 @@ def main(stdscr):
 	curses.curs_set(False)
 	computers = lib.Computers(stdscr)
 	curses.init_pair(1, curses.COLOR_RED, 0)
+	curses.halfdelay(1)
+
+	#test
+
+	l = lib.Links(computers)
+	l.remove_some()
+	l.apply(computers)
 	computers.draw() 
-	computers.list[5].chain.add(False)
-	computers.list[6].chain.add(True)
 	computers.print_content() 
-	stdscr.refresh()
-	stdscr.getkey()
+
+	a = ''
+	c = 1
+	while a != 'q':
+
+
+	
+	#computers.list[5].chain.add(False)
+	#computers.list[6].chain.add(True) 
+		if c % 2 == 0:
+			computers.add_random(False)
+
+
+		if c % 10 == 0:
+			computers.add_random(True)
+
+		c = c + 1
+
+		computers.propagate_content()
+		computers.time_shift()
+		computers.print_content() 
+		time.sleep(1)
+		stdscr.refresh()
+		try:
+			a = stdscr.getkey()
+		except:
+			pass
 
 
 
